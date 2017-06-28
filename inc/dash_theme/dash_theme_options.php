ea42b4fea5977d33589304ecdbc92f9ec072ed81
Jonathan Louback <louback@inforce.com.br>
2017-06-28 18:27:29 -0300

    testando

--- inc/dash_theme/dash_theme_options.php
+++ inc/dash_theme/dash_theme_options.php
@@ -1,165 +1,75 @@
 <?php 
-function menu_theme_options() {
-	function dashboard_theme_options() {
-		require_once '/view/options-form.php';
-	}
+// function menu_theme_options() {
+// 	function dashboard_theme_options() {
+// 		require_once '/view/options-form.php';
+// 	}
 
-	add_menu_page( 'Imobi setup', 'Imobi setup', 'administrator', 'imobi_setup_theme', 'dashboard_theme_options', 'dashicons-layout', 59 );
+// 	add_menu_page( 'Imobi setup', 'Imobi setup', 'administrator', 'imobi_setup_theme', 'dashboard_theme_options', 'dashicons-layout', 59 );
 	
-}
+// }
 
 
-add_action('admin_menu', 'menu_theme_options');
+// add_action('admin_menu', 'menu_theme_options');
 
 // ======================================================================
 
+/**
+ * Função que adiciona página de configuração no admin
+ */
+// function imobi_options_page() {
+//     add_menu_page( 'Imobi setup', 'Imobi setup', 'manage_options', 'imobi_setup_theme', 'imobi_options_page_html', 'dashicons-layout', 59 );
+// }
+ 
+/**
+ * registra a função imobi_options_page no admin_menu action hook
+ */
+// add_action( 'admin_menu', 'imobi_options_page' );
+ 
+// function imobi_options_page_html() {
+//     echo "imobi_options_page_html";
+// }
 
-/**
- * Load Jetpack compatibility file.
- */
-//require get_template_directory() . '/inc/jetpack.php';
 
-// Create Slider Post Type
-//require( get_template_directory() . '/inc/slider/slider_post_type.php' );
-
-// Create Slider
-//require( get_template_directory() . '/inc/slider/slider.php' );
-
-function wporg_settings_init() {
- // register a new setting for "wporg" page
- register_setting( 'wporg', 'wporg_options' );
- 
- // register a new section in the "wporg" page
- add_settings_section(
- 'wporg_section_developers',
- __( 'The Matrix has you.', 'wporg' ),
- 'wporg_section_developers_cb',
- 'wporg'
- );
- 
- // register a new field in the "wporg_section_developers" section, inside the "wporg" page
- add_settings_field(
- 'wporg_field_pill', // as of WP 4.6 this value is used only internally
- // use $args' label_for to populate the id inside the callback
- __( 'Pill', 'wporg' ),
- 'wporg_field_pill_cb',
- 'wporg',
- 'wporg_section_developers',
- [
- 'label_for' => 'wporg_field_pill',
- 'class' => 'wporg_row',
- 'wporg_custom_data' => 'custom',
- ]
- );
+/*
+    
+@package sunsettheme
+    
+    ========================
+        ADMIN PAGE
+    ========================
+*/
+function sunset_add_admin_page() {
+    
+    //Generate Sunset Admin Page
+    add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+    
+    //Generate Sunset Admin Sub Pages
+    add_submenu_page( 'alecaddd_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'alecaddd_sunset', 'sunset_theme_create_page' );
+    add_submenu_page( 'alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css', 'sunset_theme_settings_page');
+    
+    
+    
 }
- 
-/**
- * register our wporg_settings_init to the admin_init action hook
- */
-add_action( 'admin_init', 'wporg_settings_init' );
- 
-/**
- * custom option and settings:
- * callback functions
- */
- 
-// developers section cb
- 
-// section callbacks can accept an $args parameter, which is an array.
-// $args have the following keys defined: title, id, callback.
-// the values are defined at the add_settings_section() function.
-function wporg_section_developers_cb( $args ) {
- ?>
-    <p id="<?php echo esc_attr( $args['id'] ); ?>">
-        <?php esc_html_e( 'Follow the white rabbit.', 'wporg' ); ?>
-    </p>
-    <?php
+add_action( 'admin_menu', 'sunset_add_admin_page' );
+//Activate custom settings
+    add_action( 'admin_init', 'sunset_custom_settings' );
+function sunset_custom_settings() {
+    register_setting( 'sunset-settings-group', 'first_name' );
+    add_settings_section( 'sunset-sidebar-options', 'Sidebar Option', 'sunset_sidebar_options', 'alecaddd_sunset');
+    add_settings_field( 'sidebar-name', 'First Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options');
 }
- 
-// pill field cb
- 
-// field callbacks can accept an $args parameter, which is an array.
-// $args is defined at the add_settings_field() function.
-// wordpress has magic interaction with the following keys: label_for, class.
-// the "label_for" key value is used for the "for" attribute of the <label>.
-// the "class" key value is used for the "class" attribute of the <tr> containing the field.
-// you can add custom key value pairs to be used inside your callbacks.
-function wporg_field_pill_cb( $args ) {
- // get the value of the setting we've registered with register_setting()
- $options = get_option( 'wporg_options2' );
- // output the field
- ?>
-        <select id="<?php echo esc_attr( $args['label_for'] ); ?>" data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>" name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
-            <option value="red" <?php echo isset( $options[ $args[ 'label_for'] ] ) ? ( selected( $options[ $args[ 'label_for'] ], 'red', false ) ) : ( '' ); ?>>
-                <?php esc_html_e( 'red pill', 'wporg' ); ?>
-            </option>
-            <option value="blue" <?php echo isset( $options[ $args[ 'label_for'] ] ) ? ( selected( $options[ $args[ 'label_for'] ], 'blue', false ) ) : ( '' ); ?>>
-                <?php esc_html_e( 'blue pill', 'wporg' ); ?>
-            </option>
-        </select>
-        <p class="description">
-            <?php esc_html_e( 'You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'wporg' ); ?>
-        </p>
-        <p class="description">
-            <?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'wporg' ); ?>
-        </p>
-        <?php
+function sunset_sidebar_options() {
+    echo 'Customize your Sidebar Information';
 }
- 
-/**
- * top level menu
- */
-function wporg_options_page() {
- // add top level menu page
- add_menu_page(
- 'WPOrg',
- 'WPOrg Options',
- 'manage_options',
- 'wporg',
- 'wporg_options_page_html'
- );
+function sunset_sidebar_name() {
+    $firstName = esc_attr( get_option( 'first_name' ) );
+    echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />';
 }
- 
-/**
- * register our wporg_options_page to the admin_menu action hook
- */
-add_action( 'admin_menu', 'wporg_options_page' );
- 
-/**
- * top level menu:
- * callback functions
- */
-function wporg_options_page_html() {
- // check user capabilities
- if ( ! current_user_can( 'manage_options' ) ) {
- return;
- }
- 
- // add error/update messages
- 
- // check if the user have submitted the settings
- // wordpress will add the "settings-updated" $_GET parameter to the url
- if ( isset( $_GET['settings-updated'] ) ) {
- // add settings saved message with the class of "updated"
- add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
- }
- 
- // show error/update messages
- settings_errors( 'wporg_messages' );
- ?>
-            <div class="wrap">
-                <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-                <form action="options.php" method="post">
-                    <?php
- // output security fields for the registered setting "wporg"
- settings_fields( 'wporg' );
- // output setting sections and their fields
- // (sections are registered for "wporg", each field is registered to a specific section)
- do_settings_sections( 'wporg' );
- // output save settings button
- submit_button( 'Save Settings' );
- ?>
-                </form>
-            </div>
-            <?php
+function sunset_theme_create_page() {
+    require_once( get_template_directory() . '/inc/templates/sunset-admin.php' );
+}
+function sunset_theme_settings_page() {
+    
+    echo '<h1>Sunset Custom CSS</h1>';
+    
 }
